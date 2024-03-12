9ceb62465a341ae7b5a5db10e031242d4e5a897a
masonseftas <mason.seftas@stvincent.edu>
2024-03-12 17:39:47 -0400

    Make new branch with changes

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=c:\Users\Matthew\OneDrive - Saint Vincent College\Documents\Software Engineering Work\Hello_flask\.venv
+set VIRTUAL_ENV=C:\Users\mason\flask-tutorial\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
